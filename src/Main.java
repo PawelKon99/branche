bf08e63f4a095f2bbf6054ed3f7651b4216cede9
PawelKon99 <PawelKon99@users.noreply.github.com>
2019-10-14 20:20:33 +0200

    Implement method calculate in main class
    Calculate method use Calculator class to calculating basic math operations

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void Calculate(){
+
+        int x,y;
+
+        System.out.println("Podaj pierwsza liczbe");
+        x = scanner.nextInt();
+
+        System.out.println("Podaj druga liczbe");
+        y=scanner.nextInt();
+
+        System.out.println("Dodawanie: " + Calculator.add(x,y));
+        System.out.println("Odejmowanie: " + Calculator.subtract(x,y));
+        System.out.println("Mnozenie: " + Calculator.multiply(x,y));
+        System.out.println("Dzielenie: " + Calculator.divide(x,y));
+    }
+
+    public static void main(String[] args) {
+        Calculate();
+    }
 }
