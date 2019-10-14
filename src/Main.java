660baecd551d59cf3697838ca23228010aa7e531
PawelKon99 <PawelKon99@users.noreply.github.com>
2019-10-14 19:58:01 +0200

    Implement create user method in main class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void createUser(){
+
+        String name, lastName;
+        int age;
+
+        System.out.println("Podaj imie: ");
+        name = scanner.next();
+
+        System.out.println("Podaj Nazwisko");
+        lastName=scanner.next();
+
+        System.out.println("Podaj wiek");
+        age = scanner.nextInt();
+
+        User user = new User(name,lastName,age);
+        System.out.println("otworzono usera: " + user.toString());
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
