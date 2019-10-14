039af92188403f4c3d4e0bf8f4588276f738cf5f
PawelKon99 <PawelKon99@users.noreply.github.com>
2019-10-14 20:06:54 +0200

    Create calculator class
    Class contains methods: add/subtract/multiply/divide

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+
+    public static Integer add(Integer numberOne, Integer numberTwo) {
+        return numberOne + numberTwo;
+    }
+    public static Integer subtract(Integer numberOne, Integer numberTwo) {
+        return numberOne - numberTwo;
+    }
+    public static Integer multiply(Integer numberOne, Integer numberTwo) {
+        return numberOne * numberTwo;
+    }
+    public static Float divide(Integer numberOne, Integer numberTwo) {
+        return numberOne / (float) numberTwo;
+    }
+}
